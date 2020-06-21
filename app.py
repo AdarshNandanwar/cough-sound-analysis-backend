5149dd7d21dd1da40cdd8c2e906576fd679f3b2a
AdarshNandanwar <nandanwar.adarsh@gmail.com>
2020-06-21 10:20:21 +0530

    try4

--- app.py
+++ app.py
@@ -175,7 +175,5 @@ def classify_cough():
     return jsonify(res)
   
 # main driver function 
-if __name__ == '__main__': 
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run()
