6a0fb1676014f23e0fd5ecddb0e61af5d1b880e9
AdarshNandanwar <nandanwar.adarsh@gmail.com>
2020-06-25 16:31:12 +0530

    modify model

--- app.py
+++ app.py
@@ -138,7 +138,6 @@ def classify_cough():
         "message": ""
     }
     try:
-        print("0: ")
         if request.method == 'POST':
             # check if the post request has the file part
             if 'file' not in request.files:
